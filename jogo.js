c6e1654b26d5579bc9a72345b01d61168ef070b6
Nicolas Hengler <niko_las1934@hotmail.com>
2024-06-18 03:58:21 -0300

    Gerar posições aleatŕoias no navegador

--- jogo.js
+++ jogo.js
@@ -1,1 +1,17 @@
-alert('teste')
+var altura = 0;
+var largura = 0;
+
+/* A função foi criada para mostrar no console a alteração em tempo real do tamanho
+da tela onde o jogo irá rodar*/
+function ajusteDeTamanho(){             
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(largura, altura);
+} 
+
+ajusteDeTamanho()
+
+/* este trecho gera posições aletórias para o mosquito no game */
+var larguraX = Math.floor(Math.random() * largura)
+var alturaY = Math.floor(Math.random() * altura)
+console.log(larguraX, alturaY)
