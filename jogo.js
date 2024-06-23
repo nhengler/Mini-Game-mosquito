a5e1aeab9e4d7a35d61e3e49c9b742dc4d8f0d03
Nicolas Hengler <niko_las1934@hotmail.com>
2024-06-23 02:49:21 -0300

    o mosquito altera as posições na tela

--- jogo.js
+++ jogo.js
@@ -12,6 +12,18 @@ function ajusteDeTamanho(){
 ajusteDeTamanho()
 
 /* este trecho gera posições aletórias para o mosquito no game */
+function posicaoRandomica(){
+
 var larguraX = Math.floor(Math.random() * largura)
 var alturaY = Math.floor(Math.random() * altura)
 console.log(larguraX, alturaY)
+
+//manipulação do HTML pelo DOM
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = 'mosquito' //aplicação do estilo criado em CSS
+mosquito.style.left = larguraX + 'px'
+mosquito.style.top = alturaY + 'px'
+mosquito.style.position = 'absolute' //necessário para que as posições seja aplicadas
+document.body.appendChild(mosquito)
+}
