74ae35b96c225860e2d0a912ae9e3d0b478198ee
Nicolas Hengler <niko_las1934@hotmail.com>
2024-06-23 03:18:39 -0300

    variação de tamanho do mosquito

--- jogo.js
+++ jogo.js
@@ -21,9 +21,25 @@ console.log(larguraX, alturaY)
 //manipulação do HTML pelo DOM
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosca.png'
-mosquito.className = 'mosquito' //aplicação do estilo criado em CSS
+mosquito.className = tamanhoMosquito() //aplicação do estilo criado em CSS
 mosquito.style.left = larguraX + 'px'
 mosquito.style.top = alturaY + 'px'
 mosquito.style.position = 'absolute' //necessário para que as posições seja aplicadas
 document.body.appendChild(mosquito)
+}
+
+function tamanhoMosquito(){
+    var classe = Math.floor(Math.random() * 3)
+    switch(classe){
+        case 0:
+        return 'mosquito'
+        
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+
+
 }
